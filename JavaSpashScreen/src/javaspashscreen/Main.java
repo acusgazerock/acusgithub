000a8a7eded42582771453d0669a028c4b14abc5
acusgazerock <agus.jodi@gmail.com>
2014-12-11 10:39:51 +0700

    splash, form dalam form dan jar to exe

---
+++ JavaSpashScreen/src/javaspashscreen/Main.java
@@ -0,0 +1,23 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package javaspashscreen;
+
+/**
+ *
+ * @author acusgazerock
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Splash dialog = new Splash(new javax.swing.JFrame(), true);
+        dialog.setVisible(true);
+    }
+
+}
